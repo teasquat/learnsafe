1e7d7a52fac42dc8a26b455b0d32011de0f604f6
Lord Niels N. Horn <i.am@nilq.dk>
2016-12-12 17:47:47 +0100

    Add user manager model

--- learnsafe/learnsafe/models.py
+++ learnsafe/learnsafe/models.py
@@ -1,5 +1,32 @@
 from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager
 from django.db                  import models
+
+class AccountManager(BaseUserManager):
+    def create_user(self, email, password=None, **kwargs):
+        if not email:
+            raise ValueError("Users must have a valid email address!")
+
+        if not kwards.get("username"):
+            raise ValueError("Users must have a valid username!")
+
+        account = self.model(
+            email    = self.normalize_email(email),
+            username = kwargs.get("username")
+        )
+
+        account.set_password(password)
+        account.save()
+
+        return account
+
+    def create_superuser(self, email, password, **kwargs):
+        account = self.create_user(email, password, **kwargs)
+
+        account.is_admin = True
+        account.save()
+
+        return account
 
 class Account(AbstractBaseUser):
     email      = models.EmailField(unique=True)
